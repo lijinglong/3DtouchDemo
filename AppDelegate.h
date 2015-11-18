c6b1d58ba11add9fa76afa3590c6f72320260b7f
lijinglong <2357607191@qq.com>
2015-11-18 14:31:01 +0800

    first commit
    first commit

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  3DtouchDemo
+//
+//  Created by lijinglong on 15/11/18.
+//  Copyright © 2015年 lijinglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  3DtouchDemo
+//
+//  Created by lijinglong on 15/11/18.
+//  Copyright © 2015年 lijinglong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
